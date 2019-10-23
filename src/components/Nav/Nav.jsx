6dfe400a4f4c79bce2954ea192d5d781786b115a
vladimir <vladim.volovenko@gmail.com>
2019-10-23 15:23:58 +0300

    i did put activeClassName for links from Nav-component

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -11,19 +11,19 @@ const Nav = () => {
     <div className={s.nav}>
       
         <div className={s.item}>
-          <NavLink to='/content'>Profile</NavLink>
-        </div>
-        <div className={`${s.item} ${s.active}`}>
-          <NavLink to='/dialogs'>Messages</NavLink>
+          <NavLink to='/content' activeClassName={s.activeLink}>Profile</NavLink>
         </div>
         <div className={s.item}>
-          <NavLink to='/news'>News</NavLink>
+          <NavLink to='/dialogs' activeClassName={s.activeLink}>Messages</NavLink>
         </div>
         <div className={s.item}>
-          <NavLink to='/music'>Music</NavLink>
+          <NavLink to='/news' activeClassName={s.activeLink}>News</NavLink>
         </div>
         <div className={s.item}>
-          <NavLink to='settings'>Settings</NavLink>
+          <NavLink to='/music' activeClassName={s.activeLink}>Music</NavLink>
+        </div>
+        <div className={s.item}>
+          <NavLink to='settings' activeClassName={s.activeLink}>Settings</NavLink>
         </div>
     
     </div>
