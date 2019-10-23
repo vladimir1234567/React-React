1304a0d514d7818ba63bb10e2956c4cc8799376a
vladimir <vladim.volovenko@gmail.com>
2019-10-23 20:35:33 +0300

    i did put structure for Dialogs-component: 2 columns

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,8 +5,33 @@ import s from './Dialogs.module.css';
 const Dialogs = () => {
   return (
     <div className={s.dialogs}>
-     Dialogs
-    </div>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog}>
+          Dimych
+        </div>
+        <div className={s.dialog + ' ' + s.active}>
+          Andrey
+        </div>
+        <div className={s.dialog}>
+          Sveta
+        </div>
+        <div className={s.dialog}>
+          Sasha
+        </div>
+        <div className={s.dialog}>
+          Victor
+        </div>
+        <div className={s.dialog}>
+          Valera
+        </div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hi!</div>
+        <div className={s.message}>How are uoy&</div>
+        <div className={s.message}>yol!</div>
+      </div>
+  </div>
+    
   )
 }
 
