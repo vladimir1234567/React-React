4776dc04b8837114e81f1319609f922a629072d1
vladimir <vladim.volovenko@gmail.com>
2019-11-09 19:48:33 +0200

    added defoult into sweetch in reduser-files

--- src/Redux/dialogs-reduser.js
+++ src/Redux/dialogs-reduser.js
@@ -2,30 +2,22 @@ const UPDATE_NEW_Message_BODY = 'UPDATE_NEW_Message_BODY';
 const SEND_MESSaGE = 'SEND_MESSaGE';
 
 const dialogsReduser = (state, action) => {
-    // switch(action.type) {
-    //     case UPDATE_NEW_Message_BODY: 
-    //       state.newMessageBody = action.body;
-    //       return state;
+    switch(action.type) {
+        case UPDATE_NEW_Message_BODY: 
+          state.newMessageBody = action.body;
+          return state;
 
-    //     case SEND_MESSaGE: 
-    //       let body = state.newMessageBody;
-    //       state.newMessageBody = '';
-    //       state.messages.push({id: 6, message: body});
-    //       return state;
-
+        case SEND_MESSaGE: 
+          let body = state.newMessageBody;
+          state.newMessageBody = '';
+          state.messages.push({id: 6, message: body});
+          return state;
+        default:
+            return state;
         
-    // }
-
-   if ( action.type === UPDATE_NEW_Message_BODY ) {
-        state.newMessageBody = action.body;
-    }
-    else if ( action.type === SEND_MESSaGE ) {
-        let body = state.newMessageBody;
-        state.newMessageBody = '';
-        state.messages.push({id: 6, message: body});
     }
 
-    return state;
+
 }
 
 export default dialogsReduser;
