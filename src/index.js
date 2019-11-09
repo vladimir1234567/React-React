20a3e3c907e30033ec1b88dd912df70749276fae
vladimir <vladim.volovenko@gmail.com>
2019-11-09 22:50:41 +0200

    renamed:    src/Redux/State.js -> src/Redux/store.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom';
-import store from './Redux/State';
+import store from './Redux/store';
 
 let rerenderEntireTree = (state) => {
 ReactDOM.render(
